6de9b8cc00e6156891ea192f8e1f81bf4aa6572a
Vincent Bernardoff <vb@luminar.eu.org>
2020-01-17 13:01:46 +0100

    test: drop dead code

--- test/test.ml
+++ test/test.ml
@@ -4,32 +4,6 @@ open Async
 open Kucoin
 open Kucoin_rest
 
-module Cfg = struct
-  type cfg = {
-    key: string ;
-    secret: string ;
-    passphrase: string [@default ""];
-    quote: (string * int) list [@default []];
-  } [@@deriving sexp]
-
-  type t = (string * cfg) list [@@deriving sexp]
-end
-
-(* let default_cfg = Filename.concat (Option.value_exn (Sys.getenv "HOME")) ".virtu"
- * let cfg =
- *   List.Assoc.find_exn ~equal:String.equal
- *     (Sexplib.Sexp.load_sexp_conv_exn default_cfg Cfg.t_of_sexp) "KUCOIN" *)
-
-let () =
-  Logs.set_reporter (Logs_async_reporter.reporter ()) ;
-  Logs.set_level (Some Debug)
-
-(* let auth = {
- *   Fastrest.key = cfg.Cfg.key ;
- *   secret = Base64.decode_exn cfg.Cfg.secret ;
- *   meta = ["passphrase", cfg.Cfg.passphrase] ;
- * } *)
-
 let wrap_request
     ?(timeout=Time.Span.of_int_sec 5)
     ?(speed=`Quick) n service =
@@ -49,20 +23,6 @@ let wrap_request_light
 
 let test_later = ref []
 
-(* let accounts_full () =
- *   Fastrest.request ~auth (accounts ~sandbox:true ()) >>=? fun accts ->
- *   test_later := [
- *     "ledgers", (List.map accts ~f:begin fun a ->
- *         wrap_request ~timeout:(Time.Span.of_int_sec 10)
- *           (Format.asprintf "%a" Uuidm.pp a.id)
- *           (ledger ~sandbox:true a.id) end) ;
- *     "holds", (List.map accts ~f:begin fun a ->
- *         wrap_request ~timeout:(Time.Span.of_int_sec 10)
- *           (Format.asprintf "%a" Uuidm.pp a.id)
- *           (hold ~sandbox:true a.id) end) ;
- *   ] ;
- *   Deferred.Or_error.return accts *)
-
 let rest = [
   wrap_request "symbols" (symbols ()) ;
   wrap_request "book_sandbox"
@@ -71,10 +31,11 @@ let rest = [
     (book20 ~sandbox:false (Pair.create ~base:"BTC" ~quote:"USDT")) ;
   wrap_request "bookL3"
     (book20 ~sandbox:false (Pair.create ~base:"BTC" ~quote:"USDT")) ;
-  (* wrap_request_light "accounts" accounts_full ; *)
 ]
 
 let () =
+  Logs.set_reporter (Logs_async_reporter.reporter ()) ;
+  Logs.set_level (Some Debug) ;
   Alcotest.run ~and_exit:false "kucoin" [
     "rest", rest ;
   ] ;
